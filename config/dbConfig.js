9b8e16282b51ede68507c415374b02b6c7b22980
Siripoom <artorsiriratpoom@gmail.com>
2025-02-19 23:11:33 +0700

    edit port and host

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: 5433,
+    port: 5432,
     dialect: "postgres",
     logging: false,
   }
